25f28e6e5212a7a3ecf0588bf338ff30dc90895c
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-07-09 12:52:53 +0300

    js
    del

--- assets/js/nav-bar.js
+++
@@ -1,6 +0,0 @@
-var showSpoiler = function () {
-  document.getElementById('name1').setAttribute('checked', 'checked')
-  document.getElementById('spoiler').style.display = 'block'
-}
-
-document.getElementById('link').addEventListener('click', showSpoiler, false)

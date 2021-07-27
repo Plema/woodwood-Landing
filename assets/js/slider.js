6ff762e2ce88db2eb75cfbf4c561a762810e5c49
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-07-27 15:34:34 +0300

    add

---
+++ assets/js/slider.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: 'linear',
+    infinite: true,
+    // autoplay: false,
+    // autoplaySpeed: 5000,
+    // pauseOnDotsHover: true,
+    // pauseOnFocus: true,
+    // pauseOnHover: true,
+    // draggable: false,
+    // swipe: true,
+    // waitForAnimate: false,
+    // centerMode: false,
+    // variableWidth: false,
+    // vertical: true,
+    // verticalSwiping: true,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {},
+    //   },
+    // ],
+  })
+})
